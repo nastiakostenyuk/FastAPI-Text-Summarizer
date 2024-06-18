7a142e8e000a770189f362c77e7213f4e880795a
anastasia_kostenyuk <nastuakostenyk@gmail.com>
2024-06-18 14:39:08 +0300

    update summarize endpoint

--- utils/endpoints.py
+++ utils/endpoints.py
@@ -5,6 +5,7 @@ from utils.diff_func import summarize_text
 
 @app.post("/summarize")
 async def summarize(text_to_summarize: TextToSummarize):
+    """ Endpoint that summarizes text from a post request """
     result = await summarize_text(text_to_summarize.text)
     return {"summarized_text": result}
 
